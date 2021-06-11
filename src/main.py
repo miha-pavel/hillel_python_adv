1c1b87c0cc8370b5bbe934dd3af63869e7faebc7
miha-pavel <pahamihali4@gmail.com>
2021-06-11 19:48:25 +0300

    lesson_10: def create_list_comprehantion(old_list):

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+
+def create_list_comprehantion(old_list):
+    return [i**2 for i in old_list if i > 0]
+
 def main():
-    print('Hello World!')
+    old_list = [0, 1, 2, 3, 4, 5, 6]
+    new_list = create_list_comprehantion(old_list)
+    print('new_list: ', new_list)
+
 
 if __name__ == '__main__':
     main()
